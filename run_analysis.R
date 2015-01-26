ce243673b81391306d940b8464223495322ded1b
Alexander Pyle <alexander.pyle@gmail.com>
2015-01-25 17:29:47 -0700

    Partial Step 4 and semi-functional Step 5.

--- run_analysis.R
+++ run_analysis.R
@@ -11,43 +11,43 @@
 #
 
 library(data.table)
+#library(plyr)
 library(dplyr)
-library(plyr)
 
 run_analysis <- function () {
         
-#         #
-#         # Step 1. Merge the training and the test sets to create one data set.
-#         #
-#         
-#         # load test data into R
-#         testSubject <- read.table("./subject_test.txt", sep=" ", header = FALSE)
-#         testY <- read.table("./y_test.txt", sep=" ", header = FALSE)
-#         testX <- read.table("./X_test.txt", sep="", header = FALSE, numerals = "no.loss")
-#         
-#         # convert raw test data into data table and add Subject and Y data sets
-#         testDT <- data.table(testX)
-#         testDT$Subject <- testSubject
-#         testDT$Y <- testY
-#         
-#         
-#         # load training data into R
-#         trainSubject <- read.table("./subject_train.txt", sep=" ", header = FALSE)
-#         trainY <- read.table("./y_train.txt", sep=" ", header = FALSE)
-#         trainX <- read.table("./X_train.txt", sep="", header = FALSE, numerals = "no.loss")
-#         
-#         # convert raw training data into datatable and add Subject and Y to it
-#         trainDT <- data.table(trainX)
-#         trainDT$Subject <- trainSubject
-#         trainDT$Y <- trainY
-#         
-#         # merge the test and training data into one data.table by writing both
-#         # to a temporary file and then reading the combined results into one 
-#         # data frame
-        tmpFile = "./tmpMerge.txt"
-#         write.table(testDT, file=tmpFile, row.names=FALSE)
-#         write.table(trainDT, file=tmpFile, append=TRUE, 
-#                     row.names=FALSE, col.names=FALSE)
+        #
+        # Step 1. Merge the training and the test sets to create one data set.
+        #
+        
+        # load test data into R
+        testSubject <- read.table("./subject_test.txt", sep=" ", header = FALSE)
+        testY <- read.table("./y_test.txt", sep=" ", header = FALSE)
+        testX <- read.table("./X_test.txt", sep="", header = FALSE, numerals = "no.loss")
+        
+        # convert raw test data into data table and add Subject and Y data sets
+        testDT <- data.table(testX)
+        testDT$Subject <- testSubject
+        testDT$Y <- testY
+        
+        
+        # load training data into R
+        trainSubject <- read.table("./subject_train.txt", sep=" ", header = FALSE)
+        trainY <- read.table("./y_train.txt", sep=" ", header = FALSE)
+        trainX <- read.table("./X_train.txt", sep="", header = FALSE, numerals = "no.loss")
+        
+        # convert raw training data into datatable and add Subject and Y to it
+        trainDT <- data.table(trainX)
+        trainDT$Subject <- trainSubject
+        trainDT$Y <- trainY
+        
+        # merge the test and training data into one data.table by writing both
+        # to a temporary file and then reading the combined results into one 
+        # data frame
+       tmpFile = "./tmpMerge.txt"
+        write.table(testDT, file=tmpFile, row.names=FALSE)
+        write.table(trainDT, file=tmpFile, append=TRUE, 
+                    row.names=FALSE, col.names=FALSE)
         allData <- read.table(tmpFile, sep=" ", header = TRUE, numerals = "no.loss")
         allData <- data.table(allData)
         
@@ -74,7 +74,25 @@ run_analysis <- function () {
         #
         # Step 3. Name the activities with descriptive activity names
         #
-        
+#         myCols <- c("Time Body acceleration signal Mean X-axis",
+#                     "Time Body acceleration signal Mean Y-axis",
+#                     "Time Body acceleration signal Mean Z-axis",
+#                     "Time Body acceleration signal StdDev X-axis",
+#                     "Time Body acceleration signal StdDev Y-axis",
+#                     "Time Body acceleration signal StdDev Z-axis",
+#                     "Time Gravity acceleration signal Mean X-axis",
+#                     "Time Gravity acceleration signal Mean Y-axis",
+#                     "Time Gravity acceleration signal Mean Z-axis",
+#                     "Time Gravity acceleration signal StdDev X-axis",
+#                     "Time Gravity acceleration signal StdDev Y-axis",
+#                     "Time Gravity acceleration signal StdDev Z-axis",
+#                     "Time Body acceleration Jerk Mean X-axis",
+#                     "Time Body acceleration Jerk Mean Y-axis",
+#                     "Time Body acceleration Jerk Mean Z-axis",
+#                     "Time Body acceleration Jerk StdDev X-axis",
+#                     "Time Body acceleration Jerk StdDev Y-axis",
+#                     "Time Body acceleration Jerk StdDev Z-axis",
+#         )
         # Load the activity labels
         actLabels <- read.table("./activity_labels.txt", sep=" ", header=FALSE)
         colnames(actLabels) <- c("ActivityCode", "ActivityFactor")
@@ -83,7 +101,18 @@ run_analysis <- function () {
         tidyData <- merge(tidyData, actLabels, by="ActivityCode", all=TRUE)
         
         #
-        # Return: return the tidy data set
+        # Step 4. Label the data set with descriptive variable names
+        #
+        
+        #
+        # Step 5. Create a second, independent tidy data set with the average
+        #         of each variable for each activity and each subject.
+        #
+        tidyData <- tidyData[, lapply(.SD, ave), .SDcols=names(tidyData)[3:68], 
+                             by=list(Subject, ActivityFactor)]
+
+        #
+        # Return: return the second tidy data set
         #
         
         tidyData
